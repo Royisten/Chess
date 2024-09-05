254d50bb27b0c117f775af67f48a51054591bbf4
royisten <nemihidomine@gmail.com>
2024-09-05 08:38:46 +0530

    Day 1

--- src/main/test.java
+++ src/main/test.java
@@ -3,6 +3,6 @@ package main;
 public class test {
     public static void main(String[] args) {
         System.out.println(
-                "Isaiah 40:31 says, \"But they that wait upon the Lord shall renew their strength; they shall mount up with wings as eagles; they shall run, and not be weary; and they shall walk, and not faint\" ");
+                "Isaiah 40:31  \"But they that wait upon the Lord shall renew their strength; they shall mount up with wings as eagles; they shall run, and not be weary; and they shall walk, and not faint\" ");
     }
 }
