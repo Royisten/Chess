bdc9d0b0365daec6b3626a1c134f030dbabf1f1d
royisten <nemihidomine@gmail.com>
2024-09-07 07:17:21 +0530

    Day 1

--- chesstest/src/main/test.java
+++ chesstest/src/main/test.java
@@ -2,6 +2,7 @@ package main;
 
 public class test {
     public static void main(String[] args) {
+        //TODO ; Always Pray Before Starting.
         System.out.println(
                 "Isaiah 40:31  \"But they that wait upon the Lord shall renew their strength; they shall mount up with wings as eagles; they shall run, and not be weary; and they shall walk, and not faint\" ");
     }
