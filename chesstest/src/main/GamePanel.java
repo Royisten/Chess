c81294a2ddbcf6b6ceb3a61a95b5a318b13450bb
royisten <nemihidomine@gmail.com>
2024-09-07 08:06:58 +0530

    Day 1 / 0.1%

---
+++ chesstest/src/main/GamePanel.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import javax.swing.plaf.DimensionUIResource; //FIXME ; Switch to "Dimesion" if ui not
+
+public class GamePanel extends JPanel{
+    public static final int WIDTH = 1100;  
+    public static final int HEIGHT = 800;  
+  
+    public GamePanel (){
+        setPreferredSize(new DimensionUIResource(WIDTH,HEIGHT));
+        setBackground(Color.BLACK);
+    }
+}
---
+++ chesstest/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        //used JFrame from Swinfg to create a window!
+        JFrame window =  new JFrame ("Simple Chess");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        //added the panel
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
